3b7e4a774af0d6f6ac6619e0c1c579b2a76b2c03
Valen Rionald <vrionald@gmail.com>
2022-12-21 13:36:32 +0700

    Update CORS

--- main.go
+++ main.go
@@ -4,24 +4,11 @@ import (
 	"gin-mongo-api/configs"
 	"gin-mongo-api/routes" //add this
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://resplendent-dragon-4ca5a6.netlify.app/")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "http://sbc-sebatcabut.herokuapp.com/" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	}
-}
 
 func main() {
 	router := gin.Default()
@@ -38,6 +25,18 @@ func main() {
 	routes.SumberDayaGeologiRoute(router) //add this
 	routes.LokasiTemuanRoute(router)      //add this
 	routes.KoordinatRoute(router)         //add this
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://resplendent-dragon-4ca5a6.netlify.app"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "OPTIONS", "GET", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "Accept", "Origin", "Cache-Control", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://sbc-sebatcabut.herokuapp.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	router.Run(":" + SetPort())
 }

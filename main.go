ec1c2d1ca6e9cc01832dee42b0b67e12f77f30ac
Valen Rionald <vrionald@gmail.com>
2023-01-07 23:06:24 +0700

    Coba *

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	// - Credentials share
 	// - Preflight requests cached for 12 hours
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://resplendent-dragon-4ca5a6.netlify.app", "https://batbut.github.io", "https://museumgeologibandung.netlify.app/", "http://localhost:5173/", "https://museumgeologibandung.netlify.app/dashboard/tables"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},

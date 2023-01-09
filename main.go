076a51cc741e324114a5b8fe57276d1202d867ea
Valen Rionald <vrionald@gmail.com>
2023-01-09 15:16:35 +0700

    Cleaning Code main.go

--- main.go
+++ main.go
@@ -13,22 +13,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://resplendent-dragon-4ca5a6.netlify.app")
-// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-// 		if c.Request.Method == "https://sbc-sebatcabut.herokuapp.com" {
-// 			c.AbortWithStatus(204)
-// 			return
-// 		}
-
-// 		c.Next()
-// 	}
-// }
-
 func main() {
 	configs.ConnectDB()
 	router := gin.Default()
@@ -48,10 +32,6 @@ func main() {
 		},
 		MaxAge: 12 * time.Hour,
 	}))
-
-	// // Swagger
-	// fs := http.FileServer(http.Dir("./docs/index.html"))
-	// http.Handle("/docs/index.html/", http.StripPrefix("/docs/index.html/", fs))
 
 	routes.InvertebrataRoute(router)      //add this
 	routes.VertebrataRoute(router)        //add this
